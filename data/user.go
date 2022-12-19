389e3fd9143068184bfb095acc726b409cefa123
ryokawa <kawata.ryo.0518@gmail.com>
2022-12-19 09:02:31 +0900

    add user

--- data/user.go
+++ data/user.go
@@ -11,6 +11,30 @@ type User struct {
 	CreatedAt time.Time
 }
 
+type Session struct {
+	Id        int
+	Uuid      string
+	Email     string
+	UserId    int
+	CreatedAt time.Time
+}
+
+// Create a new session for an existing user
+func (user *User) CreateSession() (session Session, err error) {
+	// statement := "insert into sessions (uuid, email, user_id, created_at) " +
+	// 	"values ($1, $2, $3, $4) " +
+	// 	"returning id, uuid, email, user_id, created_at"
+	// stmt, err := Db.Prepare(statement)
+	// if err != nil {
+	// 	return
+	// }
+	// defer stmt.Close()
+
+	// err = stmt.QueryRow()
+	return
+}
+
+// Create a new user, Save user into the database
 func (user *User) Create() (err error) {
 	statement := "insert into users (uuid, name, email, password, created_at) " +
 		"values ($1, $2, $3, $4, $5) " +
@@ -27,6 +51,32 @@ func (user *User) Create() (err error) {
 	return
 }
 
+// update user from database
+func (user *User) Update() (err error) {
+	statement := "update users set name = $2, email = $3 where id = $1"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(user.Id, user.Name, user.Email)
+	return
+}
+
+// delete user from database
+func (user *User) Delete() (err error) {
+	statement := "delete from users where id = $1"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(user.Id)
+	return
+}
+
 // Delete all users from database
 func UserDeleteAll() (err error) {
 	statement := "delete from users"
@@ -34,10 +84,36 @@ func UserDeleteAll() (err error) {
 	return
 }
 
+// Get all users in the database and returns it
+func Users() (users []User, err error) {
+	rows, err := Db.Query("SELECT id, uuid, name, email, password, created_at FROM users")
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		user := User{}
+		err = rows.Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.Password, &user.CreatedAt)
+		if err != nil {
+			return
+		}
+		users = append(users, user)
+	}
+	return
+}
+
 // Get a single user given the email
 func UserByEmail(email string) (user User, err error) {
 	user = User{}
 	err = Db.QueryRow("SELECT id, uuid, name, email, password, created_at FROM users WHERE email = $1", email).
+		Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.Password, &user.CreatedAt)
+	return
+}
+
+// Get a single user given the UUID
+func UserByUUID(uuid string) (user User, err error) {
+	user = User{}
+	err = Db.QueryRow("SELECT id, uuid, name, email, password, created_at FROM users WHERE uuid = $1", uuid).
 		Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.Password, &user.CreatedAt)
 	return
 }
--- data/user_test.go
+++ data/user_test.go
@@ -1,6 +1,9 @@
 package data
 
-import "testing"
+import (
+	"database/sql"
+	"testing"
+)
 
 func Test_UserCreate(t *testing.T) {
 	setup()
@@ -16,5 +19,84 @@ func Test_UserCreate(t *testing.T) {
 	}
 	if users[0].Email != u.Email {
 		t.Errorf("User retrieved is not the same as the one created")
+	}
+}
+
+func Test_UserUpdate(t *testing.T) {
+	setup()
+	if err := users[0].Create(); err != nil {
+		t.Error(err, "Cannot create user")
+	}
+	users[0].Name = "Random User"
+	if err := users[0].Update(); err != nil {
+		t.Error(err, "- Cannot update user")
+	}
+	u, err := UserByEmail(users[0].Email)
+	if err != nil {
+		t.Error(err, "- Cannot get user")
+	}
+	if u.Name != "Random User" {
+		t.Error(err, "- User not updated")
+	}
+}
+
+func Test_UserDelete(t *testing.T) {
+	setup()
+	if err := users[0].Create(); err != nil {
+		t.Error(err, "Cannot create user")
+	}
+	if err := users[0].Delete(); err != nil {
+		t.Error(err, "- Cannot delete user")
+	}
+	_, err := UserByEmail(users[0].Email)
+	if err != sql.ErrNoRows {
+		t.Error(err, "- User not deleted")
+	}
+}
+
+func Test_Users(t *testing.T) {
+	setup()
+	for _, user := range users {
+		if err := user.Create(); err != nil {
+			t.Error(err, "Cannot create user")
+		}
+	}
+	us, err := Users()
+	if err != nil {
+		t.Error(err, "Cannot retrieve users.")
+	}
+	if len(us) != 2 {
+		t.Error(err, "Wrong number of users retrieved")
+	}
+	if us[0].Email != users[0].Email {
+		t.Error(us[0], users[0], "Wrong user retrieved")
+	}
+}
+
+func Test_UserByUUID(t *testing.T) {
+	setup()
+	if err := users[0].Create(); err != nil {
+		t.Error(err, "Cannot create user")
+	}
+	u, err := UserByUUID(users[0].Uuid)
+	if err != nil {
+		t.Error(err, "User not created")
+	}
+	if users[0].Email != u.Email {
+		t.Errorf("User retrieved is not the same as the one created")
+	}
+}
+
+func Test_CreateSession(t *testing.T) {
+	setup()
+	if err := users[0].Create(); err != nil {
+		t.Error(err, "Cannot create user")
+	}
+	session, err := users[0].CreateSession()
+	if err != nil {
+		t.Error(err, "Cannot create session")
+	}
+	if session.UserId != users[0].Id {
+		t.Error("User not linked with session")
 	}
 }
